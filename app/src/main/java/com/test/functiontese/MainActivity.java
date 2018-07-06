345fc0e54741d486c8e6137c0ac600825075fac4
DavyJohn <1139099003@qq.com>
2018-07-06 14:32:37 +0800

    拍照扫描身份证功能+中文

--- app/src/main/java/com/test/functiontese/MainActivity.java
+++ app/src/main/java/com/test/functiontese/MainActivity.java
@@ -1,20 +1,35 @@
 package com.test.functiontese;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.test.functiontese.camera.CameraActivity;
 import com.test.functiontese.tools.BarChartManager;
 import com.test.functiontese.tools.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private TextView time;
     private BarChart mBar;
+    private Button mSb;
+    private ImageView imageView;
     /**
      * 统计区域
      */
@@ -25,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         time = (TextView) findViewById(R.id.time);
+        mSb = findViewById(R.id.sb);
+        imageView = (ImageView) findViewById(R.id.main_image);
+
+        mSb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto(CameraActivity.TYPE_ID_CARD_FRONT);
+            }
+        });
         Utils.initDatePicker(MainActivity.this,time,false);
         mBar  = findViewById(R.id.bar_chart);
         //设置x轴的标题
@@ -66,5 +90,53 @@ public class MainActivity extends AppCompatActivity {
         mBar.animateY(800);
 
 
+    }
+    /**
+     * 拍摄证件照片
+     *
+     * @param type 拍摄证件类型
+     */
+    private void takePhoto(int type) {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0x12);
+            return;
+        }
+        CameraActivity.navToCamera(this, type);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == CameraActivity.REQUEST_CODE) {
+            //获取文件路径，显示图片
+            if (data != null) {
+                String path = data.getStringExtra("result");
+                String text = data.getStringExtra("string");
+//                String name = text.substring(text.indexOf("姓名")+2,text.indexOf("性别"));
+//                String xb = text.substring(text.indexOf("性别")+2,text.indexOf("民族"));
+//                String mz = text.substring(text.indexOf("民族")+2,text.indexOf("出生"));
+////                String cs = text.substring(text.indexOf("出生")+2,text.indexOf("住址"));
+//                String zz = text.substring(text.indexOf("住址")+2,text.indexOf("日")+1);
+//                String sfz = text.substring(text.indexOf("号码")+2,text.length());
+                Pattern pattern = Pattern.compile("[0-9]*");
+                Matcher isNum = pattern.matcher(data.getStringExtra("string").substring(text.length()-18,text.length()));
+                if (!isNum.matches()){
+                    //反的
+                    time.setText(data.getStringExtra("string").substring(0,18));
+
+                }else {
+                    time.setText(data.getStringExtra("string").substring(text.length()-18,text.length()));
+
+                }
+
+                if (!TextUtils.isEmpty(path)) {
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/test/functiontese/camera/CameraActivity.java
@@ -0,0 +1,198 @@
+package com.test.functiontese.camera;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+import com.test.functiontese.R;
+import com.test.functiontese.util.FileUtil;
+import com.test.functiontese.util.ImageFilter;
+
+
+/**
+ * Created by gxj on 2018/2/18 11:46.
+ * 拍照界面
+ */
+public class CameraActivity extends Activity implements View.OnClickListener {
+    //中文字典表
+//    static final String TESSBASE_PATH = Environment.getExternalStorageDirectory() + "/tesseract";
+    static final String TESSBASE_PATH = Environment.getExternalStorageDirectory() + "/";
+
+    //识别语言英文
+    static final String DEFAULT_LANGUAGE = "eng";
+    //中文
+//    static final String DEFAULT_LANGUAGE = "chi_sim";
+    /**
+     * 身份证正面
+     */
+    public final static int TYPE_ID_CARD_FRONT = 1;
+    /**
+     * 身份证反面
+     */
+    public final static int TYPE_ID_CARD_BACK = 2;
+
+    public final static int REQUEST_CODE = 0X13;
+
+    private CustomCameraPreview customCameraPreview;
+    private View containerView;
+    private ImageView cropView;
+    private View optionView;
+    private String content;
+    private int type;
+
+    /**
+     * 跳转到拍照页面
+     */
+    public static void navToCamera(Context context, int type) {
+        Intent intent = new Intent(context, CameraActivity.class);
+        intent.putExtra("type", type);
+        ((Activity) context).startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        type = getIntent().getIntExtra("type", 0);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_camera);
+
+        customCameraPreview = (CustomCameraPreview) findViewById(R.id.camera_surface);
+        containerView = findViewById(R.id.camera_crop_container);
+        cropView = (ImageView) findViewById(R.id.camera_crop);
+        optionView = findViewById(R.id.camera_option);
+
+        //获取屏幕最小边，设置为cameraPreview较窄的一边
+        float screenMinSize = Math.min(getResources().getDisplayMetrics().widthPixels, getResources().getDisplayMetrics().heightPixels);
+        //根据screenMinSize，计算出cameraPreview的较宽的一边，长宽比为标准的16:9
+        float maxSize = screenMinSize / 9.0f * 16.0f;
+        RelativeLayout.LayoutParams layoutParams;
+
+        layoutParams = new RelativeLayout.LayoutParams((int) maxSize, (int) screenMinSize);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        customCameraPreview.setLayoutParams(layoutParams);
+
+        float height = (int) (screenMinSize * 0.75);
+        float width = (int) (height * 75.0f / 47.0f);
+        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams((int) width, ViewGroup.LayoutParams.MATCH_PARENT);
+        LinearLayout.LayoutParams cropParams = new LinearLayout.LayoutParams((int) width, (int) height);
+        containerView.setLayoutParams(containerParams);
+        cropView.setLayoutParams(cropParams);
+        switch (type) {
+            case TYPE_ID_CARD_FRONT:
+                cropView.setImageResource(R.mipmap.camera_front);
+                break;
+            case TYPE_ID_CARD_BACK:
+                cropView.setImageResource(R.mipmap.camera_back);
+                break;
+        }
+
+        customCameraPreview.setOnClickListener(this);
+        findViewById(R.id.camera_close).setOnClickListener(this);
+        findViewById(R.id.camera_take).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.camera_surface:
+                customCameraPreview.focus();
+                break;
+            case R.id.camera_close:
+                finish();
+                break;
+            case R.id.camera_take:
+                takePhoto();
+        }
+    }
+
+    private void takePhoto() {
+        optionView.setVisibility(View.GONE);
+        customCameraPreview.setEnabled(false);
+        customCameraPreview.takePhoto(new Camera.PictureCallback() {
+            public void onPictureTaken(final byte[] data, final Camera camera) {
+                //子线程处理图片，防止ANR
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Bitmap bitmap = null;
+                        if (data != null) {
+                            bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                            camera.stopPreview();
+                        }
+                        if (bitmap != null) {
+                            //计算裁剪位置
+                            float left = ((float) containerView.getLeft() - (float) customCameraPreview.getLeft()) / (float) customCameraPreview.getWidth();
+                            float top = (float) cropView.getTop() / (float) customCameraPreview.getHeight();
+                            float right = (float) containerView.getRight() / (float) customCameraPreview.getWidth();
+                            float bottom = (float) cropView.getBottom() / (float) customCameraPreview.getHeight();
+
+                            //裁剪及保存到文件
+                            Bitmap resBitmap = Bitmap.createBitmap(bitmap,
+                                    (int) (left * (float) bitmap.getWidth()),
+                                    (int) (top * (float) bitmap.getHeight()),
+                                    (int) ((right - left) * (float) bitmap.getWidth()),
+                                    (int) ((bottom - top) * (float) bitmap.getHeight()));
+                            //新加功能
+                            FileUtil.saveBitmap(resBitmap);
+                            localre(resBitmap);
+                            if (!bitmap.isRecycled()) {
+                                bitmap.recycle();
+                            }
+                            if (!resBitmap.isRecycled()) {
+                                resBitmap.recycle();
+                            }
+
+                            //拍照完成，返回对应图片路径
+                            Intent intent = new Intent();
+                            intent.putExtra("result", FileUtil.getImgPath());
+                            intent.putExtra("string",content);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }
+                        return;
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private String localre (Bitmap bm){
+        content = "";
+        bm = bm.copy(Bitmap.Config.ARGB_8888,true);
+        TessBaseAPI baseAPI = new TessBaseAPI();
+        baseAPI.init(TESSBASE_PATH, DEFAULT_LANGUAGE);
+        baseAPI.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO);
+//        bm =  ImageFilter.gray2Binary(bm);// 图片二值化
+//        bm =  ImageFilter.grayScaleImage(bm);// 图片灰度
+        baseAPI.setImage(bm);
+        content = baseAPI.getUTF8Text();
+//        content = content.substring(content.length()-18,content.length());
+//        String str = "";
+//        for (int i=0;i<content.length();i++){
+//            if ((content.charAt(i)>=48 && content.charAt(i)<=57)||
+//                    (content.charAt(i)>=65 && content.charAt(i)<=90)){
+//                str += content.charAt(i);
+//            }
+//        }
+//        Log.e("===========>",str);
+
+        baseAPI.clear();
+        baseAPI.end();
+        return content;
+   }
+
+}
